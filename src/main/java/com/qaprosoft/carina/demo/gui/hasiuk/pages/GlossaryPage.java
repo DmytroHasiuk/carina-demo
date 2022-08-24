898cc82eb546168e278a5c7a1b05415fb45c50ac
Dmytro <dhasiuk@solvd.com>
2022-08-24 10:45:32 +0300

    added logs

--- src/main/java/com/qaprosoft/carina/demo/gui/hasiuk/pages/GlossaryPage.java
+++ src/main/java/com/qaprosoft/carina/demo/gui/hasiuk/pages/GlossaryPage.java
@@ -4,10 +4,15 @@ import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebEleme
 import com.qaprosoft.carina.demo.gui.hasiuk.components.glossary.ParagraphsLinks;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.invoke.MethodHandles;
 import java.util.List;
 
 public class GlossaryPage extends GsmArenaPage {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @FindBy(xpath = "//div[@class='st-text']/h3")
     private List<ExtendedWebElement> paragraphTitles;
@@ -29,11 +34,21 @@ public class GlossaryPage extends GsmArenaPage {
             List<String> paragraphLinksText = paragraphs.get(i).getLinksTexts();
             if (i == 0) {
                 for (String linkText : paragraphLinksText)
-                    if (!Character.isDigit(linkText.charAt(0))) return false;
+                    if (!Character.isDigit(linkText.charAt(0))) {
+                        LOGGER.error("Paragraph link : \"" + linkText + "\" does not match first letter of paragraph " +
+                                "header: \"" + paragraphTitles.get(i).getText() + "\"");
+                        return false;
+                    } else LOGGER.info("Paragraph link : \"" + linkText + "\" matches first letter of paragraph " +
+                            "header: \"" + paragraphTitles.get(i).getText() + "\"");
             } else {
                 for (String linkText : paragraphLinksText)
                     if (!(Character.toLowerCase(linkText.charAt(0)) ==
-                            Character.toLowerCase(paragraphTitles.get(i).getText().charAt(0)))) return false;
+                            Character.toLowerCase(paragraphTitles.get(i).getText().charAt(0)))) {
+                        LOGGER.error("Paragraph link : \"" + linkText + "\" does not match first letter of paragraph " +
+                                "header: \"" + paragraphTitles.get(i).getText() + "\"");
+                        return false;
+                    } else LOGGER.info("Paragraph link : \"" + linkText + "\" matches first letter of paragraph " +
+                            "header: \"" + paragraphTitles.get(i).getText() + "\"");
             }
         }
         return true;
