c74bebe6e4087cf97cb13352e6fe5833b8bf0dd8
Dmytro <dhasiuk@solvd.com>
2022-09-06 18:49:26 +0300

    finished mobile task 1

--- src/main/java/com/qaprosoft/carina/demo/mobile/gui/pages/hasiuk/pages/android/LoginPage.java
+++ src/main/java/com/qaprosoft/carina/demo/mobile/gui/pages/hasiuk/pages/android/LoginPage.java
@@ -1,8 +1,11 @@
 package com.qaprosoft.carina.demo.mobile.gui.pages.hasiuk.pages.android;
 
+import com.qaprosoft.carina.core.foundation.crypto.CryptoTool;
 import com.qaprosoft.carina.core.foundation.utils.factory.DeviceType;
 import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.qaprosoft.carina.demo.mobile.gui.pages.hasiuk.pages.common.LoginPageBase;
+import com.qaprosoft.carina.demo.mobile.gui.pages.hasiuk.pages.common.WebViewPageBase;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
@@ -12,12 +15,102 @@ public class LoginPage extends LoginPageBase {
     @FindBy(id = "action_bar_root")
     private ExtendedWebElement barTool;
 
+    @FindBy(id = "name")
+    private ExtendedWebElement inputNameField;
+
+    @FindBy(id = "password")
+    private ExtendedWebElement inputPasswordField;
+
+    @FindBy(id = "radio_male")
+    private ExtendedWebElement maleRadioButton;
+
+    @FindBy(id = "radio_female")
+    private ExtendedWebElement femaleRadioButton;
+
+    @FindBy(id = "checkbox")
+    private ExtendedWebElement privacyPolicyCheckBox;
+
+    @FindBy(id = "login_button")
+    private ExtendedWebElement singUpButton;
+
     public LoginPage(WebDriver driver) {
         super(driver);
     }
 
     @Override
-    public boolean isPageOpened(){
+    public boolean isPageOpened() {
         return barTool.isElementPresent();
+    }
+
+    public boolean isInputNameFieldPresent() {
+        return inputNameField.isPresent();
+    }
+
+    public boolean isInputPasswordFieldPresent() {
+        return inputPasswordField.isPresent();
+    }
+
+    public boolean isMaleButtonPresent() {
+        return maleRadioButton.isPresent();
+    }
+
+    public boolean isFemaleButtonPresent() {
+        return femaleRadioButton.isPresent();
+    }
+
+    public boolean isPrivacyPolicyCheckboxPresent() {
+        return privacyPolicyCheckBox.isPresent();
+    }
+
+    public boolean isPrivacyPolicyCheckboxChecked() {
+        return Boolean.parseBoolean(privacyPolicyCheckBox.getAttribute("checked"));
+    }
+
+    public void typeName(String name) {
+        inputNameField.type(name);
+    }
+
+    public void typePassword(String password) {
+        inputPasswordField.type(password);
+    }
+
+    @Override
+    public void clickMaleButton() {
+        maleRadioButton.click();
+    }
+
+    @Override
+    public void clickFemaleButton() {
+        femaleRadioButton.click();
+    }
+
+    @Override
+    public void clickPrivacyPolicyCheckbox() {
+        privacyPolicyCheckBox.click();
+    }
+
+    @Override
+    public WebViewPageBase clickSignUpButton() {
+        singUpButton.click();
+        return initPage(getDriver(), WebViewPageBase.class);
+    }
+
+    public boolean isNameTyped(String name) {
+        return StringUtils.equalsIgnoreCase(inputNameField.getText(), name);
+    }
+
+    public boolean isPasswordTyped(String password) {
+        CryptoTool cryptoTool = new CryptoTool();
+        String decryptKey = StringUtils.remove(password, "{crypt:");
+        decryptKey = StringUtils.remove(decryptKey, "}");
+        return StringUtils.equalsIgnoreCase(inputPasswordField.getText(), cryptoTool.decrypt(decryptKey));
+    }
+
+    public boolean isMaleRadioButtonChecked() {
+        return Boolean.parseBoolean(maleRadioButton.getAttribute("checked"));
+    }
+
+    public boolean isFemaleRadioButtonChecked() {
+        return Boolean.parseBoolean(femaleRadioButton.getAttribute("checked"));
     }
 }
---
+++ src/main/java/com/qaprosoft/carina/demo/mobile/gui/pages/hasiuk/pages/common/WebViewPageBase.java
@@ -0,0 +1,11 @@
+package com.qaprosoft.carina.demo.mobile.gui.pages.hasiuk.pages.common;
+
+import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+
+public abstract class WebViewPageBase extends AbstractPage {
+
+    public WebViewPageBase(WebDriver driver) {
+        super(driver);
+    }
+}
